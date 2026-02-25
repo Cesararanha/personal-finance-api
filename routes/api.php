7a872af1b19485182a34606b901b9285a4a08506
César Aranha <cesararanha2009@gmail.com>
2026-02-25 16:04:13 -0300

    feat: add API routes

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\SummaryController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+
+    Route::apiResource('categories', CategoryController::class);
+
+    Route::apiResource('transactions', TransactionController::class);
+
+    Route::get('/summary', [SummaryController::class, 'index']);
+});
